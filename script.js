bb5504928c60d7fbd0bcf6b5e6cc79ae1fa8cfde
Nisha Bukke <sononenisha827@gmail.com>
2024-03-07 10:45:26 +0530

    responsive changes

--- script.js
+++ script.js
@@ -12,6 +12,45 @@
 //   });
 // });
 
+
+// mobile menu
+
+$( document ).ready(function() {
+
+  $( ".cross" ).hide();
+  $( ".menu" ).hide();
+  $( ".canva_expander" ).click(function() {
+  $( ".menu" ).slideToggle( "slow", function() {
+  $( ".canva_expander" ).hide();
+  $( ".cross" ).show();
+  });
+  });
+  
+  $( ".cross" ).click(function() {
+  $( ".menu" ).slideToggle( "slow", function() {
+  $( ".cross" ).hide();
+  $( ".canva_expander" ).show();
+  });
+  });
+  
+  });
+
+ // Close out sub menu
+ $('.sub__close').click(function(e) {
+  e.preventDefault();
+  
+  $(this).parent().parent().removeClass('is-active');
+});
+
+// Trigger sub menu
+$('.menu ul .nav__submenu').click(function(e) {
+  e.preventDefault();
+  
+  $(this).siblings().addClass('is-active');
+});
+
+
+
 function activeTab(evt, id) {
            
   // Get all elements with class="tablinks" and remove the class "active"
